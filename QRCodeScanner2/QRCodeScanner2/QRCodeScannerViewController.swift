a4d7978bb93b866010d416a16839d81f35c3e860
TAEW00KIM <90167750+TAEW00KIM@users.noreply.github.com>
2022-08-14 17:40:24 +0900

    Add files via upload
    QRCodeScanner final

--- QRCodeScanner2/QRCodeScanner2/QRCodeScannerViewController.swift
+++ QRCodeScanner2/QRCodeScanner2/QRCodeScannerViewController.swift
@@ -7,109 +7,111 @@
 
 import UIKit
 import AVFoundation
+import QRCodeReader
 
-class QRCodeScannerViewController: UIViewController {
-    
-    var qrCodeView: UIView = {
-        var tempFrameView = UIView()
-        tempFrameView.layer.borderColor = UIColor.green.cgColor
-        tempFrameView.layer.borderWidth = 2
-        
-        return tempFrameView
-    }()
-    
-    var captureSession = AVCaptureSession()
-    var cameraDevice: AVCaptureDevice?
+class QRCodeScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        initCameraDevice()
-        initCameraInputData()
-        initCameraOutputData()
-        displayPreview()
-        initQRCodeFrameView()
-    }
 
-    private func initQRCodeFrameView() {
-        self.view.addSubview(qrCodeView)
-        self.view.bringSubviewToFront(qrCodeView)
-    }
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
 
-    private func initCameraDevice() {
-        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
-            print("Failed to get the camera video")
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
             return
         }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
         
-        cameraDevice = captureDevice
+        checkCameraPermission()
     }
 
-    
-    private func initCameraInputData() {
-        if let cameraDevice = self.cameraDevice {
-            do {
-                let input = try AVCaptureDeviceInput(device: cameraDevice)
-                if captureSession.canAddInput(input) { captureSession.addInput(input) }
-            } catch {
-                print(error.localizedDescription)
-                return
-            }
-        }
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
     }
-    
-    private func initCameraOutputData() {
-        let captureMedataOutput = AVCaptureMetadataOutput()
-        
-        if captureSession.canAddOutput(captureMedataOutput) {
-            captureSession.addOutput(captureMedataOutput)
-        }
-        
-        captureMedataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        captureMedataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-    }
-    
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    
-    private func displayPreview() {
-        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        
-        DispatchQueue.main.async {
-            self.videoPreviewLayer?.frame = self.view.layer.bounds
-            self.view.layer.addSublayer(self.videoPreviewLayer!)
-        }
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            self.captureSession.startRunning()
-        }
-    }
-}
 
-extension QRCodeScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        if metadataObjects.count == 0 {
-            self.qrCodeView.transform = CGAffineTransform(scaleX: 0, y: 0)
-            return
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
         }
-        
-        guard let metaDataObj = metadataObjects[0] as? AVMetadataMachineReadableCodeObject else {
-            print("FAIL")
-            return
-        }
-        
-        if metaDataObj.type == .qr {
-            let qrCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metaDataObj)
-            
-            self.qrCodeView.frame = qrCodeObject!.bounds
-            
-            UIView.animate(withDuration: 0.5) {
-                self.qrCodeView.transform = CGAffineTransform(scaleX: 1, y: 1)
+
+        dismiss(animated: true)
+    }
+
+    func found(code: String) {
+        print(code)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    func checkCameraPermission() {
+        AVCaptureDevice.requestAccess(for: .video, completionHandler: { (granted: Bool) in
+            if granted {
+                print("카메라 권한 허용")
+            } else {
+                print("카메라 권한 거부")
             }
-            
-            guard let qrCodeStringData = metaDataObj.stringValue else { return }
-            
-            print(qrCodeStringData)
-        }
+        })
     }
 }
