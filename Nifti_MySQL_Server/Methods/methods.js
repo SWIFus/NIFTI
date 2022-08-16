3340e91877eef7f6ab3afc82d71dddc88f44e0dd
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-08-16 19:54:14 +0900

    Create New User Method 1/2

--- Nifti_MySQL_Server/Methods/methods.js
+++ Nifti_MySQL_Server/Methods/methods.js
@@ -22,15 +22,6 @@ app.get(`/userInfo/user`, (req, res) => {
 
 
 // POST Methods
-app.post(`/AddNewUser/normal`, (req, res) => { // 회원가입 - 로그인 다시 만들기
-    const hash = giveHash(req.body.name, req.body.created_at)
-    db.query(`INSERT INTO user (hash, created_at, name, public_key, user_avatar, friend_list, password, repair_key) VALUES(?, NOW(), ?, ?, 10, ?, ?);`,
-    [])
-});
-
-
-
-
 app.post(`/userInfo/edit`, (req, res) => { // User Avatar Change
     db.query(`UPDATE user SET user_avatar=? WHERE id=?;`, [req.body.user_avatar, req.body.id], (err, data) => {
         if (!err) {
@@ -46,3 +37,61 @@ app.post(`/userInfo/edit`, (req, res) => { // User Avatar Change
         }
     });
 });
+
+
+// Add New User
+app.post(`add/user/newUser`, (req, res) => {
+    db.query(`SELECT name FROM user;`, (err, data) => {
+        if (data.find(req.body.name)) {
+            res.send({ "error": "Same User Name"})
+        }
+    })
+
+    
+    const newUserInfo = giveNewUserInfo(req.body.name, Date())
+    db.query(`INSERT INTO user (?, ?, ?, ?, ?, ?, ?, ? , ?) VALUES(unchained_hash, chained_hash, created_at, name, user_avatar, friend_list, repair_key);`,
+     [], (err) => {
+         if (!err) {
+            db.query(`SELECT * FROM user WHERE chained_hash = ?`, [newUserInfo.chained_hash], (err, data) => {
+                if (!err) {
+                    res.send(data)
+                } else {
+                    res.send(err)
+                }
+            })
+         } else {
+             res.send(err)
+         }
+     })
+})
+
+
+function giveNewUserInfo(name, created_at) {
+    const newBlock = NewBlockMakeFunction(name, created_at)
+    const repair_key = encodeString(newBlock.unchained_hash)
+    const forReturn = [newBlock.unchained_hash, newBlock.chained_hash, created_at, name,10, undefined, repair_key]
+
+    return forReturn
+}
+
+
+function encodeString(unencodedString) {
+
+
+    return unencodedString
+}
+
+/*
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| unchained_hash | text        | YES  |     | NULL    |                |
+| chained_hash   | text        | YES  |     | NULL    |                |
+| created_at     | datetime    | NO   |     | NULL    |                |
+| name           | varchar(10) | NO   |     | NULL    |                |
+| user_avatar    | int         | YES  |     | 10      |                |
+| friend_list    | text        | YES  |     | NULL    |                |
+| repair_key     | text        | NO   |     | NULL    |                |
+| id             | int         | NO   | PRI | NULL    | auto_increment |
++----------------+-------------+------+-----+---------+----------------+
+*/
